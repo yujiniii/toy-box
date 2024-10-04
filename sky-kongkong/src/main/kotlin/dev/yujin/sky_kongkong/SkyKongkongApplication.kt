6e0b9ea1a88a2cb001a2831da8386de4cc4794d3
유진 <yujiniii@icloud.com>
2024-10-04 21:22:11 +0900

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ sky-kongkong/src/main/kotlin/dev/yujin/sky_kongkong/SkyKongkongApplication.kt
@@ -0,0 +1,11 @@
+package dev.yujin.sky_kongkong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkyKongkongApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkyKongkongApplication>(*args)
+}
---
+++ sky-kongkong/src/test/kotlin/dev/yujin/sky_kongkong/SkyKongkongApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yujin.sky_kongkong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkyKongkongApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
